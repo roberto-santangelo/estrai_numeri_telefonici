d87f40b76521883c5143210389aac990723539aa
Roberto Santangelo <roberto.santangelo@hotmail.it>
2025-03-31 22:33:10 +0200

    funzionante con word

--- estrai.py
+++ estrai.py
@@ -3,8 +3,7 @@ from bs4 import BeautifulSoup
 from collections import defaultdict
 import re
 import time
-from docx import Document
-from docx.shared import Inches
+import docx
 
 comune = "Campobasso"
 indirizzi = [
@@ -17,81 +16,18 @@ indirizzi = [
     "Via Galanti 2",
     "Via Galanti 4",
 ]
-
-indirizzi = [
-    "Via Toscana 22",
-    "Via Toscana 30",
-    "Via Toscana 46",
-]
-
-# ESEMPIO PARTE DI CODICE PAGINE BIANCHE DI CUI FARE IL WEB SCRAPING
-"""
-<section class="list-element list-element--free" data-user="frls-54475848">
-    <div class="list-element__content">
-        <div class="list-element__header d-flex">
-            <div class="d-flex align-items-center ml-auto"> </div>
-        </div>
-        <div class="d-flex position-relative justify-content-between">
-            <div class="ln-3">
-                <h2 class="list-element__title ln-3 org fn"><a
-                        href="https://www.paginebianche.it/torino/fiorenzo-canavesio.dhabgdffbc"
-                        title="Canavesio Fiorenzo" class="shinystat_ssxl" data-pag="Rgs"
-                        data-tr="listing-search-itm-rag" rel="follow">Canavesio Fiorenzo<span
-                            class="list-element__expand"> </span></a></h2>
-                <div>
-                    <div class="list-element__address adr"><span><span data-tr="listing-search-itm-adr"><span
-                                    class="street-address">Via Roma 10 - </span><span>10121</span> <span
-                                    class="locality">Torino</span><span> (TO)</span></span></span></div>
-                </div>
-            </div>
-        </div>
-        <div class="list-element__footer">
-            <div class="d-flex flex-wrap mt-10">
-                <div class="phone-numbers phone-numbers--listing d-md-block d-none" data-nosnippet="true"><a
-                        data-pag="mostra telefono" href="javascript:void(0);" rel="nofollow"
-                        class="phone-numbers__cloak btn shinystat_ssxl js-toggle"><span class="icon icon--tel text-13">
-                        </span>
-                        <div class="btn__label">TELEFONO</div>
-                    </a><a data-controls="#otherPhoneNumbers" href="javascript:void(0);" rel="nofollow"
-                        class="phone-numbers__main btn hidden js-toggle">
-                        <div class="icon icon--tel"> </div>
-                        <div class="btn__label tel">370 1635512</div>
-                    </a></div><a rel="nofollow" class="phone-number-call list-element__cta btn d-md-none shinystat_ssxl"
-                    data-pag="CHIAMATA" href="tel:370 1635512"><span class="icon icon--tel"> </span>
-                    <div class="btn__label">CHIAMA</div>
-                </a>
-            </div>
-        </div>
-    </div>
-</section>
-"""
-# ESEMPIO PARTE DI CODICE INELENCO DI CUI FARE IL WEB SCRAPING
-"""
-<tr>
-    <td height=20 class="cerca" bgcolor='#FFFFFF' id='risultati'>
-        <a href="/?dir=vedi&id=10969936-privati&nome=Sandrone Rosanna ">
-            Sandrone Rosanna
-        </a>
-    </td>
-</tr>
-<tr>
-    <td height=20 class="dativ" bgcolor='#FFFFFF' id='risultati'>Telefono 0119925118</td>
-</tr>
-<tr>
-    <td bgcolor='#FFFFFF' id='risultati' height=20 class="dati">Via Roma, 6</td>
-</tr>
-<tr>
-    <td height=20 bgcolor='#FFFFFF' id='risultati' class="dati"><b>Comune</b> Mombello Di Torino</td>
-</tr>
-<tr>
-    <td height=20 bgcolor='#FFFFFF' id='risultati' class="dati"><b>Provincia</b> (TO)</td>
-</tr>
-<tr>
-    <td height=20 bgcolor='#FFFFFF' id='risultati' class="dati"><b>CAP</b> 10020</td>
-</tr>
-"""
-
-
+if False:
+    indirizzi = []
+    comune = input("INSERISCI IL COMUNE/CITTA' DI CUI VUOI CERCARE GLI INDIRIZZI: ")
+    indirizzo = None
+    i = 1
+    print(
+        "INSERISCI UN INDIRIZZO ALLA VOLTA, QUANDO HAI FINITO LASCIA IL CAMPO DI INDIRIZZO VUOTO E PREMI INVIO"
+    )
+    while indirizzo != "":
+        indirizzo = input(f"INSERISCI IL {i}° INDIRIZZO: ")
+        indirizzi.append(indirizzo)
+        i += 1
 elenco_numeri = set()  # Set per evitare duplicati più efficientemente
 numeri_di_telefono = defaultdict(list)  # Dizionario con i risultati
 
@@ -145,6 +81,11 @@ for ind in indirizzi:
                 else "Telefono non trovato"
             )
 
+            cap, comune, provincia = località.split(" ")
+            match = re.search(r"\((.*?)\)", provincia)
+            provincia = match.group(1)
+            località = f"{cap} {comune} {provincia}"
+
             if (
                 telefono not in elenco_numeri and telefono.isdigit()
             ):  # Evita numeri errati
@@ -185,10 +126,34 @@ for ind in indirizzi:
                 ):
                     continue  # SALTO SE NON VALIDO
 
-                tag = elem.find_next("tr")
+                tag = elem.find_next("tr")  # TELEFONO
                 if tag:
                     elemento_tag = tag.find("td", class_="dativ")
                 telefono = pulisci_numero(elemento_tag.get_text(strip=True))
+
+                tag = tag.find_next("tr")  # VIA
+                if tag:
+                    elemento_tag = tag.find("td", class_="dati")
+                via = elemento_tag.get_text(strip=True)
+
+                # comune, provincia, cap
+                tag = tag.find_next("tr")  # VIA
+                if tag:
+                    elemento_tag = tag.find("td", class_="dati")
+                comune = elemento_tag.contents[-1].strip()
+
+                tag = tag.find_next("tr")  # VIA
+                if tag:
+                    elemento_tag = tag.find("td", class_="dati")
+                provincia = elemento_tag.contents[-1].strip()
+                match = re.search(r"\((.*?)\)", provincia)
+                if match:
+                    provincia = match.group(1)
+
+                tag = tag.find_next("tr")  # VIA
+                if tag:
+                    elemento_tag = tag.find("td", class_="dati")
+                cap = elemento_tag.contents[-1].strip()
 
                 if (
                     telefono not in elenco_numeri and telefono.isdigit()
@@ -199,7 +164,7 @@ for ind in indirizzi:
                             "Nome": nome,
                             "Indirizzo": via,
                             "Telefono": telefono,
-                            "Località": località,
+                            "Località": f"{cap} {comune} {provincia}",
                         }
                     )
 
@@ -218,26 +183,42 @@ for ind in indirizzi:
         da += 10  # VADO ALLA PAGINA SUCCESSIVA
         # time.sleep(0.1)  # Pausa di 1 secondo tra le richieste
 
+# CERCO IL PREFISSO
+url = f"https://www.nonsolocap.it/cap?k={comune}"
+print(url)
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, "html.parser")
+    prefisso = soup.find("td", class_="d-none d-md-table-cell")
+    prefisso = prefisso.get_text(strip=True)
 
 # --- Stampa risultati ---
 if numeri_di_telefono:
-    document = Document()
+    document = docx.Document()
+    h1 = document.add_heading(f"CONTATTI TERRITORIO - {comune.upper()}\n")
+
     for sito, contatti in numeri_di_telefono.items():
-        document.add_paragraph(f"{sito}")
-        document.add_paragraph(f"{"-" * 30}")
+        paragrafo_sito = document.add_paragraph(f"{sito}\n{'-' * 30}")
         for item in contatti:
             print(sito)
             print(f"Nome: {item['Nome']}")
             print(f"Indirizzo: {item['Indirizzo']}")
-            print(f"Telefono: {item['Telefono']}")
             print(f"Località: {item['Località']}")
+            print(f"Telefono: {item['Telefono'].replace(prefisso, f'{prefisso} ')}")
             print("")
 
             document.add_paragraph(f"{item['Nome']}")
             document.add_paragraph(f"{item['Indirizzo']}")
-            document.add_paragraph(f"{item['Telefono']}")
             document.add_paragraph(f"{item['Località']}")
+            document.add_paragraph(
+                f"{item['Telefono'].replace(prefisso, f'{prefisso} ')}"
+            )
+
             document.add_paragraph(f"")
 
+print(
+    f"NUMERO DI CONTATTI TROVATI: {sum([len(v) for v in numeri_di_telefono.values()])}"
+)
+
 document.save("contatti.docx")
-print("")
